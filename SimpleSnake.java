0109cdac946f9c4616950ab081dbe16cbcf423ef
joneikholmkea <jone@kea.dk>
2024-04-26 13:19:53 +0200

    større window på 500

--- SimpleSnake.java
+++ SimpleSnake.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 
 public class SimpleSnake extends JFrame {
 
-    private static final int WINDOW_SIZE = 400;
+    private static final int WINDOW_SIZE = 500;
     private static final int DOT_SIZE = 20;
     private int x = 100;
     private int y = 200;
