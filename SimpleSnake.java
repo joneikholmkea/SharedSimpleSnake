9037c940f8580a1a8f63ab966a82909ce9e8bcdc
joneikholmkea <jone@kea.dk>
2024-04-26 14:13:53 +0200

    større window på 600

--- SimpleSnake.java
+++ SimpleSnake.java
@@ -27,6 +27,8 @@ public class SimpleSnake extends JFrame {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
+            g.setColor(Color.WHITE);
+            g.drawString("hi there you", 300,300);
             g.setColor(Color.GREEN);
             g.fillOval(x, y, DOT_SIZE, DOT_SIZE);
         }
